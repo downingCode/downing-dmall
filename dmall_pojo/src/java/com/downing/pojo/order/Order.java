6dd14d7fa9487c3ad079463c99fb61fbb2db862e
downing <downingmail@163.com>
2019-10-21 14:23:16 +0800

    订单及详情基础实体

---
+++ dmall_pojo/src/java/com/downing/pojo/order/Order.java
@@ -0,0 +1,223 @@
+package com.downing.pojo.order;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * @author downing
+ * @descript
+ */
+@Table(name = "dg_order")
+public class Order {
+
+    @Id
+    private Long id;
+    private Integer orderStatus;
+    private Integer totalCount;
+    private BigDecimal totalPrice;
+    private BigDecimal prePrice;
+    private BigDecimal postFee;
+    private BigDecimal payMoney;
+    private Integer payType;
+    private Date createTime;
+    private Date updateTime;
+    private Date consignTime;
+    private Date endTime;
+    private Date closeTime;
+    private Integer sourceType;
+    private Boolean deleted;
+    private String buyerMessage;
+    private String receiverContact;
+    private String receiverMobile;
+    private String receiverAddress;
+    private String logisticsName;
+    private String logisticsCode;
+    private String logisticsMessage;
+    private Long shopId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(Integer orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public Integer getTotalCount() {
+        return totalCount;
+    }
+
+    public void setTotalCount(Integer totalCount) {
+        this.totalCount = totalCount;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public BigDecimal getPrePrice() {
+        return prePrice;
+    }
+
+    public void setPrePrice(BigDecimal prePrice) {
+        this.prePrice = prePrice;
+    }
+
+    public BigDecimal getPostFee() {
+        return postFee;
+    }
+
+    public void setPostFee(BigDecimal postFee) {
+        this.postFee = postFee;
+    }
+
+    public BigDecimal getPayMoney() {
+        return payMoney;
+    }
+
+    public void setPayMoney(BigDecimal payMoney) {
+        this.payMoney = payMoney;
+    }
+
+    public Integer getPayType() {
+        return payType;
+    }
+
+    public void setPayType(Integer payType) {
+        this.payType = payType;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Date getConsignTime() {
+        return consignTime;
+    }
+
+    public void setConsignTime(Date consignTime) {
+        this.consignTime = consignTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public Date getCloseTime() {
+        return closeTime;
+    }
+
+    public void setCloseTime(Date closeTime) {
+        this.closeTime = closeTime;
+    }
+
+    public Integer getSourceType() {
+        return sourceType;
+    }
+
+    public void setSourceType(Integer sourceType) {
+        this.sourceType = sourceType;
+    }
+
+    public Boolean getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public String getBuyerMessage() {
+        return buyerMessage;
+    }
+
+    public void setBuyerMessage(String buyerMessage) {
+        this.buyerMessage = buyerMessage;
+    }
+
+    public String getReceiverContact() {
+        return receiverContact;
+    }
+
+    public void setReceiverContact(String receiverContact) {
+        this.receiverContact = receiverContact;
+    }
+
+    public String getReceiverMobile() {
+        return receiverMobile;
+    }
+
+    public void setReceiverMobile(String receiverMobile) {
+        this.receiverMobile = receiverMobile;
+    }
+
+    public String getReceiverAddress() {
+        return receiverAddress;
+    }
+
+    public void setReceiverAddress(String receiverAddress) {
+        this.receiverAddress = receiverAddress;
+    }
+
+    public String getLogisticsName() {
+        return logisticsName;
+    }
+
+    public void setLogisticsName(String logisticsName) {
+        this.logisticsName = logisticsName;
+    }
+
+    public String getLogisticsCode() {
+        return logisticsCode;
+    }
+
+    public void setLogisticsCode(String logisticsCode) {
+        this.logisticsCode = logisticsCode;
+    }
+
+    public String getLogisticsMessage() {
+        return logisticsMessage;
+    }
+
+    public void setLogisticsMessage(String logisticsMessage) {
+        this.logisticsMessage = logisticsMessage;
+    }
+
+    public Long getShopId() {
+        return shopId;
+    }
+
+    public void setShopId(Long shopId) {
+        this.shopId = shopId;
+    }
+}
---
+++ dmall_pojo/src/java/com/downing/pojo/order/OrderDetail.java
@@ -0,0 +1,150 @@
+package com.downing.pojo.order;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+
+/**
+ * @author downing
+ * @descript
+ */
+@Table(name = "dg_order_detail")
+public class OrderDetail {
+
+    @Id
+    private Long id;
+    private String orderCode;
+    private Long spuId;
+    private Long skuId;
+    private String goodsName;
+    private String title;
+    private BigDecimal unitPrice;
+    private Integer count;
+    private BigDecimal totalPrice;
+    private BigDecimal payMoney;
+    private String images;
+    private Boolean isReturn;
+    private BigDecimal postFee;
+    private BigDecimal discount;
+    private Long shopId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOrderCode() {
+        return orderCode;
+    }
+
+    public void setOrderCode(String orderCode) {
+        this.orderCode = orderCode;
+    }
+
+    public Long getSpuId() {
+        return spuId;
+    }
+
+    public void setSpuId(Long spuId) {
+        this.spuId = spuId;
+    }
+
+    public Long getSkuId() {
+        return skuId;
+    }
+
+    public void setSkuId(Long skuId) {
+        this.skuId = skuId;
+    }
+
+    public String getGoodsName() {
+        return goodsName;
+    }
+
+    public void setGoodsName(String goodsName) {
+        this.goodsName = goodsName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public BigDecimal getPayMoney() {
+        return payMoney;
+    }
+
+    public void setPayMoney(BigDecimal payMoney) {
+        this.payMoney = payMoney;
+    }
+
+    public String getImages() {
+        return images;
+    }
+
+    public void setImages(String images) {
+        this.images = images;
+    }
+
+    public Boolean getReturn() {
+        return isReturn;
+    }
+
+    public void setReturn(Boolean aReturn) {
+        isReturn = aReturn;
+    }
+
+    public BigDecimal getPostFee() {
+        return postFee;
+    }
+
+    public void setPostFee(BigDecimal postFee) {
+        this.postFee = postFee;
+    }
+
+    public BigDecimal getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(BigDecimal discount) {
+        this.discount = discount;
+    }
+
+    public Long getShopId() {
+        return shopId;
+    }
+
+    public void setShopId(Long shopId) {
+        this.shopId = shopId;
+    }
+}
